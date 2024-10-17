bc955e96076322cc896f6a654e6ba2323037a817
BIP Kohan and Belikov <deniskochan01@gmail.com>
2024-10-17 16:22:19 +0300

    добавлен метод сложения

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 
-public class Calculator{
+public  class Calculator{
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        int a = scan.nextInt();
+        System.out.println("Введите второое число");
+        int b = scan.nextInt();
+        System.out.println("Введите действие:+, -, *, /, %");
+        String c = scan.next();
+        switch (c){
+            case "+":
+                System.out.println("Сумма: " + summ(a,b));
+                break;
+
+        }
 
     }
-
+    public static int summ(int a, int b){
+        return a + b;
+    }
 }
